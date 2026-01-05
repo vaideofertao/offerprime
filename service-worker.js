f986fac0f68fe211eb3059e157b8974e400a8d01
vaideofertao <hleopinheiro@outlook.com>
2026-01-05 05:58:40 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "offerprime-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/site.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
